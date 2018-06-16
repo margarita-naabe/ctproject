9eb1c353dc39f219b7a92ac55c19b42da055a6b7
Naa <margaritanaabe.mn@gmail.com>
2018-06-16 16:10:29 +0100

    initial commit

--- controllers/default.py
+++ controllers/default.py
@@ -9,36 +9,31 @@ def store():
     submitted_password = request.vars.password
 
     results = db.users.insert(
-      db_studentname = submitted_studentname,
-      db_id = submitted_id,
-      db_email= submitted_email,
-      db_password= submitted_password,
-     )
+           db_studentname=submitted_studentname,
+           db_id=submitted_id,
+           db_email=submitted_email,
+           db_password=submitted_password
+    )
 
     if results:
         return "User Saved Successfully"
     else:
-         return "An error Occured"
-
+        return "An Error Occured"
 
 def seeUsers():
-    users=db().select(db.users.ALL) 
-    return dict(users=users)  
-
+    users=db().select(db.users.ALL)
+    return dict(users=users)
 
 def login():
     return dict()
-
 
 def authenticate():
     submitted_email = request.vars.email
     submitted_password = request.vars.password
 
     if db(db.users.db_email==submitted_email
-              and db.users.db_password==submitted_password) .count()>0:
-        return "User Logged in Successfully" 
-    else:
-        return "User Not Found.Please Sign Up"    
-        
+            and db.users.db_password==submitted_password).count()>0:
+        return "User Logged in Successfully"
 
-             
+    else:
+        return "User Not found. Please Sign up"
